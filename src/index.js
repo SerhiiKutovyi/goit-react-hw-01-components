45c85d29458daaba80f9e041f80b180a66666422
SerhiiKutovyi <wagenrex@gmail.com>
2023-01-13 18:48:40 +0200

    Start

--- src/index.js
+++ src/index.js
@@ -1,24 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-const element = React.createElement('div', {
-  a: 5,
-  b: 10,
-  children: 'hello world',
-});
-
-const jsxElement = <div>hello world</div>;
-console.log(jsxElement);
-
-// ReactDOM.createRoot(element, document.querySelector('#root'));
-
-// import ReactDOM from 'react-dom/client';
-// // import { App } from 'components/App';
-// import './index.css';
+import { App } from 'components/App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  element
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
