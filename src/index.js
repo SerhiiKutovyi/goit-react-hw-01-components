815dd05947584359411901a37615676ffb70568d
SerhiiKutovyi <wagenrex@gmail.com>
2023-01-13 14:25:55 +0200

    test

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+
+const element = React.createElement('div', {
+  a: 5,
+  b: 10,
+  children: 'hello world',
+});
+
+const jsxElement = <div>hello world</div>;
+console.log(jsxElement);
+
+// ReactDOM.createRoot(element, document.querySelector('#root'));
+
+// import ReactDOM from 'react-dom/client';
+// // import { App } from 'components/App';
+// import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  element
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
 );
